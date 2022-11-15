bd6bbc30e4c4855680ea54b62c0d3462f30bdaf0
Vis Cognitionis <omeralemdar94@gmail.com>
2022-11-16 01:59:24 +0300

    Add axios-hooks (data)

--- src/components/pages/comparison_page.js
+++ src/components/pages/comparison_page.js
@@ -1,18 +1,36 @@
 import React from "react";
+import useAxios from "axios-hooks";
+
 import ProductCard from "../product-card/card";
-import "../pages/comparison_page.scss";
 import useWindowSize from "../../custom-hooks/useWindowSize";
+import "../pages/comparison_page.scss";
 
 export default function ComparisonPage() {
   const size = useWindowSize();
 
+  const [{ data, loading, error }, refetch] = useAxios(
+    "https://fakestoreapi.com/products"
+  );
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error!</p>;
+
+  console.log(data);
   return (
     <div className="pageContainer">
       <div
         className="cardsContainer"
         style={{ width: `calc(${size.width}px - 180px)` }}
       >
-        {Array(10).fill(<ProductCard />)}
+        {data.map((item) => {
+          return (
+            <ProductCard
+              key={item.id}
+              productType={item.category}
+              productName={item.title}
+              productPrice={item.price}
+            />
+          );
+        })}
       </div>
     </div>
   );
