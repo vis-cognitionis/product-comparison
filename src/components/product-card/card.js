c4a9c570d56e95032c3dfecacab0730d6b54ad8a
Vis Cognitionis <omeralemdar94@gmail.com>
2022-11-16 01:11:26 +0300

    Refactor card styles

--- src/components/product-card/card.js
+++ src/components/product-card/card.js
@@ -2,5 +2,19 @@ import React from "react";
 import "../product-card/card.scss";
 
 export default function ProductCard() {
-  return <div className="cardContainer"></div>;
+  return (
+    <div className="cardContainer">
+      <img className="productImage" src="./logo512.png" alt=""></img>
+      <div className="productInfosContainer">
+        <p className="productType"> jewelery</p>
+        <div className="productInfos">
+          <p className="productName"> White Gold Plated Princess</p>
+          <p className="productPrice"> 9.99$</p>
+        </div>
+      </div>
+      <button className="productButton">
+        <p className="buttonName"> COMPARE</p>
+      </button>
+    </div>
+  );
 }
